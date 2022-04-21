f2014e88b46063e815d4acbb23d2f97a31f205eb
johnanderson84 <johnanderson8484@gmail.com>
2022-04-21 11:37:45 +0100

    - Refactored some of the coding for the main menu class - Added comments to other team members understand the code

--- Blackjack Project/Assets/Scripts/MainMenu.cs
+++ Blackjack Project/Assets/Scripts/MainMenu.cs
@@ -5,15 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+   /// <summary>
+   /// Function used to allow the user to move to the game selection scene on the website
+   /// </summary>
     public void ChooseGame()
     {
         SceneManager.LoadScene("GameSelection");
     }
+    /// <summary>
+    /// Function allows the user to load the blackjack scene on the website
+    /// </summary>
     public void PlayBJ()
     {
         SceneManager.LoadScene("GameScene");
     }
-
+    /// <summary>
+    /// Allows the user to return to the main menu when inside a game scene
+    /// </summary>
     public void BacktoMenu()
     {
         SceneManager.LoadScene("MainMenu");
